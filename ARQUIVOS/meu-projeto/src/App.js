5e284a8b576f06c1f6404dcdce7063d2f6a2ed0a
Lorena200216 <midia.faustino@scania.com>
2024-01-26 07:47:14 -0300

    atualizando

--- ARQUIVOS/meu-projeto/src/App.js
+++ ARQUIVOS/meu-projeto/src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-    <h1>Olá React</h1>
-    <p> Meu primeiro projeto</p>
+     <h1>Olá React</h1>
+     <p>
+    Meu primeiro projeto
+     </p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
